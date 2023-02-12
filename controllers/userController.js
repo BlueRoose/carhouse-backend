b19f33e877cbd5bf5b600e212318c4b89767f3a2
Egor Zhuk <royalecool460@gmail.com>
2023-02-12 13:25:57 +0300

    some changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,18 @@ const ApiError = require("../error/ApiError");
 
 class UserController {
   async checkUser(req, res, next) {
-    const { password } = req.body;
+    const { password } = req.query;
     const user = await User.findOne({ where: { password } });
     if (!user) {
       return next(ApiError.internalRequest("User not found"));
     }
+    return res.json(user);
+  }
+
+  async createUser(req, res) {
+    const { password, role } = req.body;
+    const user = await User.create({password, role});
+
     return res.json(user);
   }
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@ const Router = require("express");
 const router = new Router();
 const UserController = require("../controllers/userController");
 
+router.post("/", UserController.createUser)
 router.get("/auth", UserController.checkUser);
 
 module.exports = router;
